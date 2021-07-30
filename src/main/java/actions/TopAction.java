1e824e54e6fd90fd17be4b27f9a7d0d7c1f52ecf
shuta.hayakawa <shayakawa@yamatane.co.jp>
2021-07-30 13:35:21 +0900

    トップページアクションの作成

--- src/main/java/actions/TopAction.java
+++ src/main/java/actions/TopAction.java
@@ -4,22 +4,34 @@
 package actions;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 
+import actions.views.EmployeeView;
+import actions.views.ReportView;
 import constants.AttributeConst;
 import constants.ForwardConst;
+import constants.JpaConst;
+import services.ReportService;
 
 /**
  * @author ssg60
  *
  */
 public class TopAction extends ActionBase {
+
+    private ReportService service;
+
     @Override
     public void process() throws ServletException, IOException {
 
+        service = new ReportService();
+
         //メソッドを実行
         invoke();
+
+        service.close();
 
     }
 
@@ -27,6 +39,21 @@ public class TopAction extends ActionBase {
      * 一覧画面を表示する
      */
     public void index() throws ServletException, IOException {
+
+        EmployeeView loginEmployee = (EmployeeView) getSessionScope(AttributeConst.LOGIN_EMP);
+
+        int page = getPage();
+        List<ReportView> reports = service.getMinePerPage(loginEmployee, page);
+
+        long myReportsCount = service.countAllMine(loginEmployee);
+
+        putRequestScope(AttributeConst.REPORTS, reports); //取得した日報データ
+        putRequestScope(AttributeConst.REP_COUNT, myReportsCount); //ログイン中の従業員が作成した日報の数
+        putRequestScope(AttributeConst.PAGE, page); //ページ数
+        putRequestScope(AttributeConst.MAX_ROW, JpaConst.ROW_PER_PAGE); //1ページに表示するレコードの数
+
+
+
 
         //セッションにフラッシュメッセージが設定されている場合はリクエストスコープに移し替え、セッションからは削除する
         String flush = getSessionScope(AttributeConst.FLUSH);
